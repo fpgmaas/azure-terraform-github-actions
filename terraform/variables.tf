8eda57ff23d5c5d72180b97d288897a98647784a
Florian Maas <fpgmaas@gmail.com>
2023-04-01 12:45:14 +0200

    added main.tf and variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "hobbes"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "westeurope"
+}
